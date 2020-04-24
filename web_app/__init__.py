2db1cdc05be373a00f8bdffc0935b2d421334e30
Lord-Kanzler <climberalex@gmail.com>
2020-04-23 21:53:58 -0400

    deploy to heroku

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,5 +1,7 @@
 # web_app/__init__.py
 
+import os
+from dotenv import load_dotenv
 from flask import Flask
 
 from web_app.models import db, migrate
@@ -9,16 +11,26 @@ from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.admin_routes import admin_routes
 from web_app.routes.stats_routes import stats_routes
 
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
+
 
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY
 
     #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///web_app_11.db"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////mnt/c/Github/DS-Unit-3-Sprint-3-Productization-and-Cloud/tweetoff/web_app_13.db"
+    #app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////mnt/c/Github/DS-Unit-3-Sprint-3-Productization-and-Cloud/tweetoff/web_app_13.db"
 
+    # configure the database
+    app.config["SQALCHEMY_DATABASE_URL"] = DATABASE_URL
+    app.config["SQALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
     migrate.init_app(app, db)
 
+    # configure routes
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     app.register_blueprint(twitter_routes)
